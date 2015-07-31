a4058a1555e74f6f3f545bb8bd59c68a605663bf
Ezio Auditore da Firenze <quickfingersdev@gmail.com>
2015-07-31 11:41:04 -0400

    setting up project

---
+++ JSONParsing/JSONParsing/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JSONParsing
+//
+//  Created by Efthemios Suyat on 7/31/15.
+//  Copyright (c) 2015 Efthemiosprime. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
