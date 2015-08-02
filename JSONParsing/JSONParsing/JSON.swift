907072c78969e8df20903d13c7234c0195f4733b
Ezio Auditore da Firenze <quickfingersdev@gmail.com>
2015-08-01 23:58:43 -0400

    implementing usage of JSON and JSONParser

--- JSONParsing/JSONParsing/JSON.swift
+++ JSONParsing/JSONParsing/JSON.swift
@@ -28,6 +28,14 @@ public class JSON
         else {return nil}
     }
     
+    subscript(key:String) -> JSON? {
+        if let item = parsedObject as? NSDictionary {
+            if item[key] != nil {
+                return JSON(parsedObject: item[key]!)
+            }
+        }
+        return nil
+    }
     
     var stringValue:String? {get {return parsedObject as? String}}
     var intValue:Int? {get {return parsedObject as? Int}}
--- JSONParsing/JSONParsing/ViewController.swift
+++ JSONParsing/JSONParsing/ViewController.swift
@@ -12,6 +12,24 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        var fileError, jsonError: NSError?
+        
+        var jsonData = NSData(contentsOfFile: NSBundle.mainBundle().pathForResource("players", ofType: "json")!, options:nil, error: &fileError)
+        if let err = fileError 
+        {
+            println("Error: Could not load JSON file: \(fileError?.localizedDescription)")
+            return
+        }
+        
+        var json = JSONParser.parse(jsonData!, error: &jsonError)
+        
+        if let j = json {
+            println(j[0]?["name"]?.stringValue)
+            println(j[0]?["number"]?.intValue)
+        } else {
+            println("Error: Could not parse JSON. \(jsonError?.localizedDescription)")
+        }
     }
 
     override func didReceiveMemoryWarning() {
