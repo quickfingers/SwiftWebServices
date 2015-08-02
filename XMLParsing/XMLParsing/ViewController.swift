3acca3a96b3e94603fb57d291983bbbfc18801d3
Ezio Auditore da Firenze <quickfingersdev@gmail.com>
2015-08-02 01:03:28 -0400

    adding xmlparsing project

---
+++ XMLParsing/XMLParsing/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  XMLParsing
+//
+//  Created by efthemios on 8/2/15.
+//  Copyright (c) 2015 efthemios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
